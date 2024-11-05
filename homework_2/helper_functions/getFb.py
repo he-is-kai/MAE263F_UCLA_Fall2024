e77c18734c4c18d253509906ff045efe1dd6c5fa
limhekaiUCLA <108039460+limhekaiUCLA@users.noreply.github.com>
2024-11-05 14:51:17 -0800

    quick lint format change

--- homework_2/helper_functions/getFb.py
+++ homework_2/helper_functions/getFb.py
@@ -1,32 +1,39 @@
 import numpy as np
 from .gradEb_hessEb import gradEb_hessEb
 
+
 def getFb(q, m1, m2, kappaBar, EI, voronoiRefLen):
-  ndof = len(q)
-  nv = int((ndof + 1) / 4 ) # Number of vertices
-  ne = nv - 1 # Number of edges
+    ndof = len(q)
+    nv = int((ndof + 1) / 4)  # Number of vertices
+    ne = nv - 1  # Number of edges
 
-  Fb = np.zeros(ndof)
-  Jb = np.zeros((ndof,ndof))
+    Fb = np.zeros(ndof)
+    Jb = np.zeros((ndof, ndof))
 
-  for c in range(1,ne): # Loop over all the internal nodes
-    node0 = q[4 * c -4 : 4 * c - 1] # (c-1) th node
-    node1 = q[4 * c : 4 * c + 3] # c-th node
-    node2 = q[4 * c + 4: 4 * c + 7] # (c+1)-th node
-    l_k = voronoiRefLen[c]
-    kappaBar_c = kappaBar[c, :] # I made a mistake in class. I wrote kappaBar[c] instead of kappaBar[c,:]
+    for c in range(1, ne):  # Loop over all the internal nodes
+        node0 = q[4 * c - 4 : 4 * c - 1]  # (c-1) th node
+        node1 = q[4 * c : 4 * c + 3]  # c-th node
+        node2 = q[4 * c + 4 : 4 * c + 7]  # (c+1)-th node
+        l_k = voronoiRefLen[c]
+        kappaBar_c = kappaBar[
+            c, :
+        ]  # I made a mistake in class. I wrote kappaBar[c] instead of kappaBar[c,:]
 
-    m1e = m1[c-1, 0:3]
-    m2e = m2[c-1, 0:3]
-    m1f = m1[c, 0:3]
-    m2f = m2[c, 0:3]
+        m1e = m1[c - 1, 0:3]
+        m2e = m2[c - 1, 0:3]
+        m1f = m1[c, 0:3]
+        m2f = m2[c, 0:3]
 
-    ind = np.arange(4*c - 4, 4 * c + 7) # 11 elements (3 nodes, 2 edges/theta angles)
+        ind = np.arange(
+            4 * c - 4, 4 * c + 7
+        )  # 11 elements (3 nodes, 2 edges/theta angles)
 
-    dF, dJ = gradEb_hessEb(node0, node1, node2, m1e, m2e, m1f, m2f, kappaBar_c, l_k, EI)
-    # I made a mistake in class: I got the order of the inputs wrong in gradEb_hessEb. I swapped the order of l_k and EI
+        dF, dJ = gradEb_hessEb(
+            node0, node1, node2, m1e, m2e, m1f, m2f, kappaBar_c, l_k, EI
+        )
+        # I made a mistake in class: I got the order of the inputs wrong in gradEb_hessEb. I swapped the order of l_k and EI
 
-    Fb[ind] -= dF
-    Jb[np.ix_(ind, ind)] -= dJ
+        Fb[ind] -= dF
+        Jb[np.ix_(ind, ind)] -= dJ
 
-  return Fb, Jb
+    return Fb, Jb
--- homework_2/helper_functions/getRefTwist.py
+++ homework_2/helper_functions/getRefTwist.py
@@ -1,12 +1,13 @@
 import numpy as np
 from .computeReferenceTwist import computeReferenceTwist
 
+
 def getRefTwist(a1, tangent, refTwist):
-  ne = a1.shape[0] # Shape of a1 is (ne,3)
-  for c in np.arange(1,ne):
-    u0 = a1[c-1,0:3] # reference frame vector of previous edge
-    u1 = a1[c,0:3] # reference frame vector of current edge
-    t0 = tangent[c-1,0:3] # tangent of previous edge
-    t1 = tangent[c,0:3] # tangent of current edge
-    refTwist[c] = computeReferenceTwist(u0, u1, t0, t1, refTwist[c])
-  return refTwist
+    ne = a1.shape[0]  # Shape of a1 is (ne,3)
+    for c in np.arange(1, ne):
+        u0 = a1[c - 1, 0:3]  # reference frame vector of previous edge
+        u1 = a1[c, 0:3]  # reference frame vector of current edge
+        t0 = tangent[c - 1, 0:3]  # tangent of previous edge
+        t1 = tangent[c, 0:3]  # tangent of current edge
+        refTwist[c] = computeReferenceTwist(u0, u1, t0, t1, refTwist[c])
+    return refTwist
